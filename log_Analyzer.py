ab4ce6d8965f6d26f0330a95390892e773a79544
Declan Keller <kebodmk@gmail.com>
2026-04-23 10:33:13 -0400

    add brute-force detection

--- log_Analyzer.py
+++ log_Analyzer.py
@@ -22,8 +22,10 @@ try:
                 ip = parts[1].strip()
                 status = parts[2].strip()
 
-                if status == 'FAILED':
-                    failed_attempts[ip] = failed_attempts.get(ip, 0) + 1
+                if ip not in failed_attempts:
+                    failed_attempts[ip] = []
+
+                failed_attempts[ip].append(timestamp)
 
     # --- JSON FILE ---
     elif filename.endswith('.json'):
@@ -35,8 +37,10 @@ try:
                 ip = entry["ip"]
                 status = entry["status"]
 
-                if status == 'FAILED':
-                    failed_attempts[ip] = failed_attempts.get(ip, 0) + 1
+                if ip not in failed_attempts:
+                    failed_attempts[ip] = []
+
+                failed_attempts[ip].append(timestamp)
 
     # --- CSV FILE ---
     elif filename.endswith('.csv'):
@@ -48,8 +52,10 @@ try:
                 ip = row["ip"]
                 status = row["status"]
 
-                if status == 'FAILED':
-                    failed_attempts[ip] = failed_attempts.get(ip, 0) + 1
+                if ip not in failed_attempts:
+                    failed_attempts[ip] = []
+
+                failed_attempts[ip].append(timestamp)
 
     else:
         print("This file is not supported, try another.")
@@ -63,11 +69,25 @@ except json.JSONDecodeError:
 
 print("\nSuspicious IPs:")
 
+from datetime import datetime
+print("\nRapid Attempt Detection:")
+
+for ip, times in failed_attempts.items():
+    # convert to datetime objects
+    times = [datetime.strptime(t, "%Y-%m-%d %H:%M:%S") for t in times]
+    times.sort()
+
+    for i in range(len(times) - 2):
+        if (times[i + 2] - times[i]).seconds <= 30:
+            print(f"{ip} shows multiple failed attempts (possible brute-force)\n")
+            break
+
 # Sort by most failed attempts
-sorted_ips = sorted(failed_attempts.items(), key=lambda x: x[1], reverse=True)
+sorted_ips = sorted(failed_attempts.items(), key=lambda x: len(x[1]), reverse=True)
 
 # Print suspicious IPs
-for ip, count in sorted_ips:
+for ip, times in sorted_ips:
+    count = len(times)
     if count >= 3:
         print(ip, "is suspicious with", count, "failed attempts")
 
@@ -75,6 +95,7 @@ for ip, count in sorted_ips:
 with open("Suspicious_ips.txt", "w") as output:
     output.write("Suspicious IPs:\n")
 
-    for ip, count in sorted_ips:
+    for ip, times in sorted_ips:
+        count = len(times)
         if count >= 3:
             output.write(f"{ip} is suspicious with {count} failed attempts\n")
